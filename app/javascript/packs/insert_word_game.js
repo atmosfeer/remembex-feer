68aa6e54595a85214a0bcb4daa80b4a2d9f9cdca
Nicolas Feer <atmosfeer@gmail.com>
2019-08-29 19:46:36 +0200

    final js fix

--- app/javascript/packs/insert_word_game.js
+++ app/javascript/packs/insert_word_game.js
@@ -1,6 +1,50 @@
-function checkForm() {
+function countInputs() {
   var inputs = document.querySelectorAll('.insert_word_input');
-  return inputs.length === 0;
+  return inputs.length;
+}
+
+function wrapUp() {
+  console.log("Wrapping up");
+  fetch(`/game_sessions/${gameSessionId}/final_score`)
+  .then( response => response.json())
+  .then( data => {
+  let finalScore = document.querySelector("#game1_popup_window h3");
+  finalScore.innerHTML = `You had ${data.finalScore} out of ${data.inputs}`;
+  });
+  $('#game1_popup_window').slideToggle("slow");
+  $('.game1_popup_window_background').slideToggle("slow");
+}
+
+function changeInput(input, gameSessionId) {
+    const guess = input.value;
+    const answer = input.dataset.answer;
+    const requestBody = {
+      guess: guess,
+      answer: answer
+    }
+  fetch(`/game_sessions/${gameSessionId}/guess_word`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: "POST",
+    body: JSON.stringify(requestBody)
+  })
+    .then( response => response.json())
+    .then( data => {
+      if (data.success) {
+        const wrapper = document.createElement('span');
+        wrapper.classList.add('right-answer-input');
+        wrapper.innerHTML = answer;
+        input.replaceWith(wrapper);
+        console.log("correct");
+      } else {
+        const wrapper = document.createElement('span');
+        wrapper.classList.add('wrong-answer-input');
+        wrapper.innerHTML = answer;
+        input.replaceWith(wrapper);
+      }
+    })
+
 }
 
 const form = document.getElementById("insert_word_game_form");
@@ -15,50 +59,15 @@ inputs.forEach((input) => {
   input.onkeypress = function(e){
     const wordNumber = parseInt(e.currentTarget.id.match(/\d+/)[0], 10);
     if (e.keyCode === 13) {
+      changeInput(input, gameSessionId);
       const nextInput = document.getElementById("word" + (wordNumber + 1))
-      nextInput.focus();
+      if (nextInput) {
+        nextInput.focus();
+      } else {
+        wrapUp();
+      }
     }
   }
-  input.addEventListener('blur', (event) => {
-    const guess = event.currentTarget.value;
-    const answer = event.currentTarget.dataset.answer;
-    const requestBody = {
-      guess: guess,
-      answer: answer
-    }
-    fetch(`/game_sessions/${gameSessionId}/guess_word`, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: "POST",
-      body: JSON.stringify(requestBody)
-    })
-      .then( response => response.json())
-      .then( data => {
-        if (data.success) {
-          const wrapper = document.createElement('span');
-          wrapper.classList.add('right-answer-input');
-          wrapper.innerHTML = answer;
-          input.replaceWith(wrapper);
-          console.log("correct");
-        } else {
-          const wrapper = document.createElement('span');
-          wrapper.classList.add('wrong-answer-input');
-          wrapper.innerHTML = answer;
-          input.replaceWith(wrapper);
-        }
-        if (checkForm()) {
-          fetch(`/game_sessions/${gameSessionId}/final_score`)
-          .then( response => response.json())
-          .then( data => {
-          let finalScore = document.querySelector("#game1_popup_window h3");
-          finalScore.innerHTML = `You had ${data.finalScore} out of ${data.inputs}`;
-          });
-          $('#game1_popup_window').slideToggle("slow");
-          $('.game1_popup_window_background').slideToggle("slow");
-        }
-      })
-  });
 });
 
 
